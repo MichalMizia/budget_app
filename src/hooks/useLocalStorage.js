255a33791766ff282b7fbdf250ac81cfe46e996b
Mike420O <mmizia05@gmail.com>
2023-04-19 09:03:55 +0200

    Renamed files

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,8 +2,11 @@ import { useEffect, useState } from "react"
 
 export function useLocalStorage(key, defaultValue){
     const [value, setValue] = useState(()=>{
-        const jsonVal = localStorage.getItem(key)
-        if(jsonVal != null) return JSON.parse(jsonVal)
+        let jsonVal = undefined
+        if(typeof window !== "undefined"){
+            jsonVal = localStorage.getItem(key)
+        }
+        if(jsonVal !== undefined) return JSON.parse(jsonVal)
 
         if(typeof defaultValue === "function") return defaultValue()
         return defaultValue
